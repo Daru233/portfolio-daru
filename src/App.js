e35a88e9fed0000bc521fcc249e8cf15615e79bb
Michael Malto <michaeldm233@gmail.com>
2022-07-10 10:44:55 +0100

    move project to root dir

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello Heroku!</p>
+                <p>Hello Heroku</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
